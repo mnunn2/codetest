2ebf91acc8c9d7f7f3120f9e14cee2dc130a92e7
mnunn2 <bonnie650@gmail.com>
2020-06-07 14:35:19 +0100

    removed comments on foo branch

--- index.php
+++ index.php
@@ -1,11 +1,9 @@
 <?php declare(strict_types=1);
-// i was changed in b
 
 require 'vendor/autoload.php';
 
 use Fred\Order;
 use Fred\OrderCurrencyDec;
-// i was added in a
 $orderNumber = 'fyi-12346'; // sourced from sanitized user input
 try {
     $order = new Order($orderNumber);
@@ -13,7 +11,5 @@ try {
     require 'view/order_view.php';
 }
 catch(Exception $e) { // added in b
-  echo 'Message: ' . $e->getMessage();
+  echo 'Messages: ' . $e->getMessage();
 }
-// i was later added in b
-
